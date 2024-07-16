bc064ce7a35e92bc90bff743b713856a48f70887
Aniketwagh123 <106876220+Aniketwagh123@users.noreply.github.com>
2024-07-16 15:12:39 +0530

    [Aniket Wagh] Add: script to display user input in upper and lower cases

---
+++ strings/06_display_input_cases.py
@@ -0,0 +1,8 @@
+def display_input_cases():
+    user_input = input("Enter a string: ")
+    print("Upper case:", user_input.upper())
+    print("Lower case:", user_input.lower())
+
+
+if __name__ == "__main__":
+    display_input_cases()
