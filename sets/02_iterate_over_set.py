cd8d2ed89836257fb81d2f2af7fc63d6648096e0
Aniketwagh123 <106876220+Aniketwagh123@users.noreply.github.com>
2024-07-17 14:48:07 +0530

    [Aniket Wagh]Add: python script to iterate over sets

---
+++ sets/02_iterate_over_set.py
@@ -0,0 +1,9 @@
+def iterate_over_set(my_set):
+    for item in my_set:
+        print(item)
+
+
+if __name__ == "__main__":
+    my_set = {1, 2, 3, 4, 5}
+    print("Iterating over set:")
+    iterate_over_set(my_set)
