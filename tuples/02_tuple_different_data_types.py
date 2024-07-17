fc9a6629d5126dbe6ecceff48b12a10eb2f5d17e
Aniketwagh123 <106876220+Aniketwagh123@users.noreply.github.com>
2024-07-17 13:32:47 +0530

    [Aniket Wagh]Add: python script to create a tuple with different data types

---
+++ tuples/02_tuple_different_data_types.py
@@ -0,0 +1,6 @@
+def create_tuple_different_data_types():
+    return (1, 'hello', 3.5, True)
+
+if __name__ == "__main__":
+    my_tuple = create_tuple_different_data_types()
+    print("Tuple with different data types:", my_tuple)
